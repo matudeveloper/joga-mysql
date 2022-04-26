e435fa64dca8e7727ca328d5dea63f0923c8f7bc
Martin Laan <martin.laan@khk.ee>
2022-04-26 21:52:53 +0300

    Displayed an article by slug

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ app.engine('hbs', hbs.engine({
 }));
 // setup static public directory
 app.use(express.static('public'));
-
 const mysql = require('mysql');
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
-
 // create database connection
 let con = mysql.createConnection ({
     host: "localhost",
@@ -26,16 +24,14 @@ let con = mysql.createConnection ({
     password: "qwerty",
     database: "joga_mysql"
 });
-
 con.connect(function(err) {
     if(err) throw err;
     console.log('Connected to joga_mysql db')
 });
-
 // show all articles - index page
 app.get('/', (req, res) => {
     let query = "SELECT * FROM article";
-    let articles = [];
+    let articles = []
     con.query(query, (err, result) => {
         if (err) throw err;
         articles = result
@@ -43,6 +39,16 @@ app.get('/', (req, res) => {
     })
 })
 
+// show articles by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        article = result
+        res.render('article', {article: article})
+    });
+})
 
 // app start point
 app.listen (3000, () => {
