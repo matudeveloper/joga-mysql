313a5d5549d3f687facf6cf0a3c5e696756fb29e
Martin Laan <martin.laan@khk.ee>
2022-04-26 21:50:57 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -16,20 +16,35 @@ app.engine('hbs', hbs.engine({
 app.use(express.static('public'));
 
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
+
 // create database connection
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwerty',
-    database: 'joga_mysql'
+let con = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
 });
-con.connect(function (err) {
+
+con.connect(function(err) {
     if(err) throw err;
-    console.log('Connected to joga_mysql db');
+    console.log('Connected to joga_mysql db')
 });
+
+// show all articles - index page
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {articles: articles})
+    })
+})
+
+
 // app start point
-app.listen(3000, () => {
+app.listen (3000, () => {
     console.log('App is started at http://localhost:3000');
 });
