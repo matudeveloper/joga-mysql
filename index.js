990f737d093e9e15c5efb50d7de4d9ebcea05b41
Martin Laan <martin.laan@khk.ee>
2022-04-26 21:58:05 +0300

    Displayed author article list

--- index.js
+++ index.js
@@ -41,16 +41,30 @@ app.get('/', (req, res) => {
 
 // show articles by this slug
 app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.slug, article.name, article.image, article.body, article.published, author.names FROM article INNER JOIN author ON article.author_id=author.id WHERE slug="${req.params.slug}"`
+    let query = `SELECT article.slug, article.name, article.image, article.body, article.published, author.names, author.id AS author_id FROM article INNER JOIN author ON article.author_id=author.id WHERE slug="${req.params.slug}"`
     let article;
     con.query(query, (err, result) => {
         if(err) throw err;
-        console.log(result)
-        article = result
-        res.render('article', {article: article})
+    @@ -56,6 +56,21 @@ app.get('/article/:slug', (req, res) => {
     });
-})
+    })
+
+// show articles on author page
+    app.get('/author/:author_id', (req, res) => {
+        let query = `SELECT article.slug, article.name, article.image, article.body, article.published, author.names, author.id AS author_id FROM article INNER JOIN author ON article.author_id=author.id WHERE author_id="${req.params.author_id}"`;
+        let articles = [];
+        let author;
+        con.query(query, (err, result) => {
+            if(err) throw err;
+            articles = result;
+            author = result[0];
+            res.render('author', {articles: articles, author: author})
+        })
+    })
+
+//WHERE id="${req.params.id}
+
 // app start point
-app.listen (3000, () => {
-    console.log('App is started at http://localhost:3000');
-});
+    app.listen (3000, () => {
+        console.log('App is started at http://localhost:3000');
+    });
